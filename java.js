123d1d6c8220c44f644f0ec52cfc058efbcc7722
CuzFriday <laqwchaos@gmail.com>
2024-06-21 09:30:19 -0500

    New Pages!

--- java.js
+++ java.js
@@ -1,12 +1,1 @@
-
-window.onscroll = function() { 
-    var scrolled = window.scrollY; 
-    var documentHeight = document.documentElement.scrollHeight;
-    var windowHeight = window.innerHeight;
-
-    // Detect if near bottom
-    if (windowHeight + scrolled > documentHeight - 20) {  
-        // Near bottom of the page, do something
-        console.log("Near bottom of page");
-    };  
-};
+// Whenever I actually want to use javascript in the java.js file lmao
