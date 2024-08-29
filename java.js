56dfaa441723575cde7c53aa6e2d4e28e83a3507
CuzFriday <laqwchaos@gmail.com>
2024-08-29 01:59:35 -0500

    Animations Addition

--- java.js
+++ java.js
@@ -1,1 +1,77 @@
-// Whenever I actually want to use javascript in the java.js file lmao
+document.addEventListener('DOMContentLoaded', function() {
+    // Select all elements with a specific class
+    const LogoElement = document.querySelectorAll('.logo');
+    const RightTextOfLogoElement = document.querySelectorAll('.namelogo-right')
+    const DividerElement = document.querySelectorAll('.divider')
+    const cursorBubble = document.querySelector('.cursor-bubble');
+
+    let prevScrollPos = window.scrollY;
+
+    // Function to change opacity based on scroll direction
+    window.addEventListener('scroll', function() {
+        
+        const currentScrollPos = window.scrollY;
+        const scrollDirection = currentScrollPos > prevScrollPos ? 'down' : 'up';
+
+        LogoElement.forEach(element => {
+            if (scrollDirection === 'down') {
+                element.style.opacity = 0; 
+            } else {
+                element.style.opacity = 1; 
+            }
+        });
+
+        prevScrollPos = currentScrollPos;
+    });
+
+    window.addEventListener('scroll', function() {
+
+        const scrollFromTop = window.scrollY;
+        
+        RightTextOfLogoElement.forEach(element => {
+            const opacity = scrollFromTop / 550; 
+            element.style.opacity = opacity > 1 ? 1 : opacity; 
+
+            const scaleFactor = 1 + (scrollFromTop / 40000); 
+            element.style.transform = `scale(${scaleFactor})`;
+
+            element.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
+
+       
+        });
+    })
+
+    window.addEventListener('scroll', function() {
+
+        const scrollFromTop = window.scrollY;
+        
+        DividerElement.forEach(element => {
+            const marginValue = scrollFromTop / -10;
+
+            const opacity = scrollFromTop / 550; 
+            element.style.opacity = opacity > 1 ? 1 : opacity; 
+
+            element.style.marginLeft = `${marginValue}px`;
+       
+            element.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
+        });
+    })
+
+    document.addEventListener('mousemove', function(e) {
+
+        const bubbleWidth = cursorBubble.offsetWidth; 
+        const bubbleHeight = cursorBubble.offsetHeight;
+
+        const cursorX = e.pageX;
+        const cursorY = e.pageY;
+
+        // Calculate the scroll offsets
+        const scrollX = window.scrollX || window.pageXOffset;
+        const scrollY = window.scrollY || window.pageYOffset;
+
+        // Update the position of the cursor bubble considering the scroll offsets and bubble width
+        cursorBubble.style.left = cursorX - scrollX - bubbleWidth / 2 + 'px';
+        cursorBubble.style.top = cursorY - scrollY - bubbleHeight / 2 + 'px';
+
+    });
+});
